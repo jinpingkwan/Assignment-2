8e9b7255405bb47176f5150918f114807cac2b61
lokepeien <lokepeien9@gmail.com>
2025-06-10 16:04:03 +0800

    ...

--- cart.php
+++ cart.php
@@ -8,83 +8,118 @@
     <link href="https://fonts.googleapis.com/css2?family=Unica+One&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Exo:wght@400;600&display=swap" rel="stylesheet">
     <style>
-        body{
-            display: inline;
+        .box{
+           
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
             color: white;
-        }
+            scroll-margin-top: 80px;
+            padding: 20px;
+            }
 
+        #image_section, #customize_section, .box{
+            scroll-margin-top: 80px;
+        }
         #image_section{
             width: 50%;
         }
+
 
         #image_section img{
             width: 75%;
             height: auto;
         }
-
-        /* #customize_section{
-            disp
-        } */
+        #customize_section{
+            position: absolute;
+             top: 50;
+            right: 0; 
+           font-family: 'Times New Roman', Times, serif;
+        }
     </style>
     <title>Document</title>
 </head>
 <body>
+    <!-- Header section containing logo, brand name, and navigation -->
+    <div class = "header_container">
+        <div class = "header_logo">
+        <img id = "header" src="images/image (12).png" alt="logo" width = 50px height = 50px>
+        </div>
+        <div class = "header_brand_name">
+            <h1 id = "header">Chatea</h1>
+        </div>
+        <div class = "header_menu">
+            <a href="menu.php" class = "header_menu">menu</a>
+        </div>
+        <div class = "header_about_us">
+            <a href="#about_us" class = "header_about_us">about us</a>
+        </div>
+    </div>
+    <div class="box">
+        <?php
+        $id = $_POST["item_id"];
+        $name = $_POST["item_name"];
+        $image_src = $_POST["item_image"];
+
+        $image = "<img src='$image_src' alt = '$name'>";
+
+        echo "<div id = 'image_section'> $image </div>";
+
+        //Right Side of Page
+        echo "<div id = 'customize_section'>";
+        echo "<h1>$name</h1>";
+        echo "<form action = 'order.php' method = 'POST'>";
+        //Customise Sugar Level
+            echo "<div id = 'sweetness_section'>";
+                echo "<h2>Sugar Level</h2>";
+                echo "<input type='radio' id='sugar_100' name='sugar_level' value='100'>";
+                echo "<label for='sugar_100'>100%</label>";
+
+                echo "<input type='radio' id='sugar_75' name='sugar_level' value='75'>";
+                echo "<label for='sugar_75'>75%</label>";
+
+                echo "<input type='radio' id='sugar_50' name='sugar_level' value='50'>";
+                echo "<label for='sugar_50'>50%</label>";
+
+                echo "<input type='radio' id='sugar_25' name='sugar_level' value='25'>";
+                echo "<label for='sugar_25'>25%</label>";
+
+                echo "<input type='radio' id='sugar_0' name='sugar_level' value='0'>";
+                echo "<label for='sugar_0'>0%</label>";
+
+            echo "</div>";
+
+            //Customise Ice Level
+            echo "<div id = 'ice_section'>";
+                echo "<h2>Ice Level</h2>";
+                echo "<input type='radio' id='ice_100' name='ice_level' value='100'>";
+                echo "<label for='ice_100'>100%</label>";
+
+                echo "<input type='radio' id='ice_75' name='ice_level' value='75'>";
+                echo "<label for='ice_75'>75%</label>";
+
+                echo "<input type='radio' id='ice_50' name='ice_level' value='50'>";
+                echo "<label for='ice_50'>50%</label>";
+
+                echo "<input type='radio' id='ice_25' name='ice_level' value='25'>";
+                echo "<label for='ice_25'>25%</label>";
+
+                echo "<input type='radio' id='ice_0' name='ice_level' value='0'>";
+                echo "<label for='ice_0'>0%</label>";
+            echo "</div>";
+
+            //Customise Quantity
+            echo "<div id = 'quantity_section'>";
+                echo "<h2>Quantity</h2>";
+                echo "<input type = 'number' name = 'quantity'>";
+            echo "</div>";
+
+            echo "<input type='hidden' name='item_id' value = $id>";
+
+            echo "<button type='submit'>Add To Cart</button>";
+        echo "</form></div>";
+        ?>
+    </div>
     
-    <?php
-    $id = $_POST["item_id"];
-    $name = $_POST["item_name"];
-    $image_src = $_POST["item_image"];
-
-    $image = "<img src='$image_src' alt = '$name'>";
-
-    echo "<div id = 'image_section'> $image </div>";
-    echo "<div id = 'customize_section'>";
-    echo "<h1>$name</h1>";
-    echo "<form action = 'order.php' method = 'POST'>";
-        echo "<div id = 'sweetness_section>";
-            echo "<h2>Sugar Level</h2>";
-            echo "<input type='radio' id='sugar_100' name='sugar_level' value='100'>";
-            echo "<label for='sugar_100'>100%</label>";
-
-            echo "<input type='radio' id='sugar_75' name='sugar_level' value='75'>";
-            echo "<label for='sugar_75'>75%</label>";
-
-            echo "<input type='radio' id='sugar_50' name='sugar_level' value='50'>";
-            echo "<label for='sugar_50'>50%</label>";
-
-            echo "<input type='radio' id='sugar_25' name='sugar_level' value='25'>";
-            echo "<label for='sugar_25'>25%</label>";
-
-            echo "<input type='radio' id='sugar_0' name='sugar_level' value='0'>";
-            echo "<label for='sugar_0'>0%</label>";
-
-        echo "</div>";
-
-        echo "<div id = 'ice_section'>";
-            echo "<h2>Ice Level</h2>";
-            echo "<input type='radio' id='ice_100' name='ice_level' value='100'>";
-            echo "<label for='ice_100'>100%</label>";
-
-            echo "<input type='radio' id='ice_75' name='ice_level' value='75'>";
-            echo "<label for='ice_75'>75%</label>";
-
-            echo "<input type='radio' id='ice_50' name='ice_level' value='50'>";
-            echo "<label for='ice_50'>50%</label>";
-
-            echo "<input type='radio' id='ice_25' name='ice_level' value='25'>";
-            echo "<label for='ice_25'>25%</label>";
-
-            echo "<input type='radio' id='ice_0' name='ice_level' value='0'>";
-            echo "<label for='ice_0'>0%</label>";
-        echo "</div>";
-
-        echo "<div id = 'quantity_section'>";
-            echo "<h2>Quantity</h2>";
-            echo "<input type = 'number' name = 'quantity'>";
-        echo "</div>";
-
-        echo "<button type='submit'>Add To Cart</button>";
-    echo "</form></div>";
-    ?>
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -41,117 +41,156 @@
     <!-- Menu category -->
         <div class = "menu_list">
             <div class = "menu_category">
-                <form id = "form_101" action="cart.php" method = "post" onclick="document.getElementById('form_101').submit();">
-                    <div class = "category1">
-                        <img src="images/Chatea/1.png" alt="">
-                    <h3>Milk Tea</h3>
-                    </div>
-                    <input type="hidden" name="item_id" value = "101">
-                    <input type="hidden" name="item_name" value = "Milk Tea 1">
-                    <input type="hidden" name="item_image" value = "images/Chatea/1.png">
-                    
-                </form>
+                
                     
 
-                <a href="#pure_tea">
-                    <div class = "category2">
-                        <img src="images/Chatea/11.png" alt="">
-                        <h3>pure_tea</h3>
-                    </div>
-                </a>
-
-               <h1 class =  "brief">Click to know more about our tea  </h1>
-               
-               
-               <a href="#fruit_tea">
-                    <div class = "category3">
-                        <img src="images/Chatea/27.png" alt="">
-                        <h3>Fruit Tea</h3>
-                    </div>
-                </a>
-
-                <a href ="#new_launch">
-                    <div class = "category4">
-                        <img src="images/Chatea/24.png" alt="">
-                        <h3>New Launch</h3>
-                    </div>
-                 </a>
-                </div>
+                
                 
                 
            
     
 
             <div id = "milk_tea">
-                <div class ="menu_item1">
-                    <img src="images/Chatea/1.png" alt="milketea1"  >
-                    <h3>GOLDEN GUAN YIN MILK TEA</h3><br>
-                    <h3>RM 13.99</h3>
-                </div>
-                <div class ="menu_item2">
-                    <img src="images/Chatea/2.png" alt="milketea2"  >
-                    <h3>PEACH OOLONG MILK TEA</h3><br>
-                    <h3>RM 13.99</h3>
-                </div>
-                <div class ="menu_item3">
-                    <img src="images/Chatea/3.png" alt="milketea3" >
-                    <h3>ROASTED OOLONG MILK TEA</h3><br>
-                    <h3>RM 13.99</h3>
-                </div>
-                <div class ="menu_item4">
-                    <img src="images/Chatea/4.png" alt="milketea4">
-                    <h3>JASMINE GREEN MILK TEA</h3><br>
-                    <h3>RM 13.99</h3>
-                </div> 
+
+                <form id = "form_101" action="cart.php" method = "post" onclick="document.getElementById('form_101').submit();">
+                    <div class ="menu_item1">
+                        <img src="images/Chatea/1.png" alt="GOLDEN GUAN YIN MILK TEA"  >
+                        <h3>GOLDEN GUAN YIN MILK TEA</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value = "101">
+                    <input type="hidden" name="item_name" value = "GOLDEN GUAN YIN MILK TEA">
+                    <input type="hidden" name="item_image" value = "images/Chatea/1.png">
+                    
+                </form>
+                
+                <form id="form_102" action="cart.php" method="post" onclick="document.getElementById('form_102').submit();">
+                    <div class="menu_item2">
+                        <img src="images/Chatea/2.png" alt="milketea2">
+                        <h3>PEACH OOLONG MILK TEA</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="102">
+                    <input type="hidden" name="item_name" value="PEACH OOLONG MILK TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/2.png">
+                </form>
+
+                <form id="form_103" action="cart.php" method="post" onclick="document.getElementById('form_103').submit();">
+                    <div class="menu_item3">
+                        <img src="images/Chatea/3.png" alt="milketea3">
+                        <h3>ROASTED OOLONG MILK TEA</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="103">
+                    <input type="hidden" name="item_name" value="ROASTED OOLONG MILK TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/3.png">
+                </form>
+
+                <form id="form_104" action="cart.php" method="post" onclick="document.getElementById('form_104').submit();">
+                    <div class="menu_item4">
+                        <img src="images/Chatea/4.png" alt="milketea4">
+                        <h3>JASMINE GREEN MILK TEA</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="104">
+                    <input type="hidden" name="item_name" value="JASMINE GREEN MILK TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/4.png">
+                </form>
             </div>
 
             
             <div id = "pure_tea">
-                <div class ="menu_item5">
-                    <img src="images/Chatea/13.png" alt="fruittea1"  >
-                    <h3>GLUTINOUS OOLONG TEA MACCHIATO</h3><br>
-                    <h3>RM 13.99</h3>
-                </div>
-                <div class ="menu_item6">
-                    <img src="images/Chatea/10.png" alt="fruitetea2"  >
-                    <h3>HIGHLAND OOLONG TEA</h3><br>
-                    <h3>RM 13.99</h3>
-                </div>
-                <div class ="menu_item7">
-                    <img src="images/Chatea/11.png" alt="fruitetea3" >
-                    <h3>HIGHLAND OOLONG COLD BREW</h3><br>
-                    <h3>RM 13.99</h3>
-                </div>
-                <div class ="menu_item8">
-                    <img src="images/Chatea/12.png" alt="fruittea4">
-                    <h3>GOLDEN GUAN YIN TEA MACCHIATO</h3><br>
-                    <h3>RM 13.99</h3>
-                </div> 
+                <form id="form_105" action="cart.php" method="post" onclick="document.getElementById('form_105').submit();">
+                    <div class="menu_item5">
+                        <img src="images/Chatea/13.png" alt="fruittea1">
+                        <h3>GLUTINOUS OOLONG TEA MACCHIATO</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="105">
+                    <input type="hidden" name="item_name" value="GLUTINOUS OOLONG TEA MACCHIATO">
+                    <input type="hidden" name="item_image" value="images/Chatea/13.png">
+                </form>
+
+                <form id="form_106" action="cart.php" method="post" onclick="document.getElementById('form_106').submit();">
+                    <div class="menu_item6">
+                        <img src="images/Chatea/10.png" alt="fruitetea2">
+                        <h3>HIGHLAND OOLONG TEA</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="106">
+                    <input type="hidden" name="item_name" value="HIGHLAND OOLONG TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/10.png">
+                </form>
+
+                <form id="form_107" action="cart.php" method="post" onclick="document.getElementById('form_107').submit();">
+                    <div class="menu_item7">
+                        <img src="images/Chatea/11.png" alt="fruitetea3">
+                        <h3>HIGHLAND OOLONG COLD BREW</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="107">
+                    <input type="hidden" name="item_name" value="HIGHLAND OOLONG COLD BREW">
+                    <input type="hidden" name="item_image" value="images/Chatea/11.png">
+                </form>
+
+                <form id="form_108" action="cart.php" method="post" onclick="document.getElementById('form_108').submit();">
+                    <div class="menu_item8">
+                        <img src="images/Chatea/12.png" alt="fruittea4">
+                        <h3>GOLDEN GUAN YIN TEA MACCHIATO</h3><br>
+                        <h3>RM 13.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="108">
+                    <input type="hidden" name="item_name" value="GOLDEN GUAN YIN TEA MACCHIATO">
+                    <input type="hidden" name="item_image" value="images/Chatea/12.png">
+                </form> 
             </div>
             
 
            
             <div id = "fruit_tea">
-                <div class ="menu_item9">
-                    <img src="images/Chatea/25.png" alt="puretea1"  >
-                    <h3>FRESH GRAPEFRUIT TEA</h3><br>
-                    <h3>RM 14.99</h3>
-                </div>
-                <div class ="menu_item10">
-                    <img src="images/Chatea/26.png" alt="puretea2"  >
-                    <h3>JASMINE GRAPE TEA</h3><br>
-                    <h3>RM 14.99</h3>
-                </div>
-                <div class ="menu_item11">
-                    <img src="images/Chatea/27.png" alt="puretea3" >
-                    <h3>FRESH MANGO POMELO TEA</h3><br>
-                    <h3>RM 14.99</h3>
-                </div>
-                <div class ="menu_item12">
-                    <img src="images/Chatea/28.png" alt="puretea4">
-                    <h3>FRESH PASSION FRUIT TEA</h3><br>
-                    <h3>RM 14.99</h3>
-                </div> 
+                <form id="form_109" action="cart.php" method="post" onclick="document.getElementById('form_109').submit();">
+                    <div class="menu_item9">
+                        <img src="images/Chatea/25.png" alt="puretea1">
+                        <h3>FRESH GRAPEFRUIT TEA</h3><br>
+                        <h3>RM 14.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="109">
+                    <input type="hidden" name="item_name" value="FRESH GRAPEFRUIT TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/25.png">
+                </form>
+
+                <form id="form_110" action="cart.php" method="post" onclick="document.getElementById('form_110').submit();">
+                    <div class="menu_item10">
+                        <img src="images/Chatea/26.png" alt="puretea2">
+                        <h3>JASMINE GRAPE TEA</h3><br>
+                        <h3>RM 14.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="110">
+                    <input type="hidden" name="item_name" value="JASMINE GRAPE TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/26.png">
+                </form>
+
+                <form id="form_111" action="cart.php" method="post" onclick="document.getElementById('form_111').submit();">
+                    <div class="menu_item11">
+                        <img src="images/Chatea/27.png" alt="puretea3">
+                        <h3>FRESH MANGO POMELO TEA</h3><br>
+                        <h3>RM 14.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="111">
+                    <input type="hidden" name="item_name" value="FRESH MANGO POMELO TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/27.png">
+                </form>
+
+                <form id="form_112" action="cart.php" method="post" onclick="document.getElementById('form_112').submit();">
+                    <div class="menu_item12">
+                        <img src="images/Chatea/28.png" alt="puretea4">
+                        <h3>FRESH PASSION FRUIT TEA</h3><br>
+                        <h3>RM 14.99</h3>
+                    </div>
+                    <input type="hidden" name="item_id" value="112">
+                    <input type="hidden" name="item_name" value="FRESH PASSION FRUIT TEA">
+                    <input type="hidden" name="item_image" value="images/Chatea/28.png">
+                </form> 
             </div>
 
             <div id = "new_launch">
