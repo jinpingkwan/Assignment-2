49daaa7f5da10568b8a65c1b5ee8c737c9312a90
jinpingkwan <147835982+jinpingkwan@users.noreply.github.com>
2025-06-12 16:51:08 +0800

    order

--- insert_tea.php
+++ insert_tea.php
@@ -11,23 +11,23 @@ if(!$conn){
      echo "auto_increment set to 101<br>";
  }
 
-$sql_insert = "INSERT INTO tea(tea_name, tea_price) VALUES
-                            ('GOLDEN GUAN YIN MILK TEA', 13.99),
-                            ('PEACH OOLONG MILK TEA', 13.99),
-                            ('ROASTED OOLONG MILK TEA', 13.99),
-                            ('JASMINE GREEN MILK TEA', 13.99),
-                            ('GLUTINOUS OOLONG TEA MACCHIATO', 13.99),
-                            ('HIGHLAND OOLONG TEA', 13.99),
-                            ('HIGHLAND OOLONG COLD BREW', 13.99),
-                            ('GOLDEN GUAN YIN TEA MACCHIATO', 13.99),
-                            ('FRESH GRAPEFRUIT TEA', 14.99),
-                            ('JASMINE GRAPE TEA', 14.99),
-                            ('FRESH MANGO POMELO TEA', 14.99),
-                            ('FRESH PASSION FRUIT TEA', 14.99),
-                            ('FRESH WATERMELON TEA', 15.99),
-                            ('FRESH TROPICAL FRUIT TEA', 15.99),
-                            ('FRESH ORANGE TEA', 15.99),
-                            ('FRESH KIWI TEA', 15.99);";
+$sql_insert = "INSERT INTO tea(tea_name, tea_price, tea_image) VALUES
+                            ('GOLDEN GUAN YIN MILK TEA', 13.99, 'images/Chatea/1.png'),
+                            ('PEACH OOLONG MILK TEA', 13.99, 'images/Chatea/2.png'),
+                            ('ROASTED OOLONG MILK TEA', 13.99, 'images/Chatea/3.png'),
+                            ('JASMINE GREEN MILK TEA', 13.99, 'images/Chatea/4.png'),
+                            ('GLUTINOUS OOLONG TEA MACCHIATO', 13.99, 'images/Chatea/13.png'),
+                            ('HIGHLAND OOLONG TEA', 13.99, 'images/Chatea/10.png'),
+                            ('HIGHLAND OOLONG COLD BREW', 13.99, 'images/Chatea/11.png'),
+                            ('GOLDEN GUAN YIN TEA MACCHIATO', 13.99, 'images/Chatea/12.png'),
+                            ('FRESH GRAPEFRUIT TEA', 14.99, 'images/Chatea/25.png'),
+                            ('JASMINE GRAPE TEA', 14.99, 'images/Chatea/26.png'),
+                            ('FRESH MANGO POMELO TEA', 14.99, 'images/Chatea/27.png'),
+                            ('FRESH PASSION FRUIT TEA', 14.99, 'images/Chatea/28.png'),
+                            ('FRESH WATERMELON TEA', 15.99, 'images/Chatea/21.png'),
+                            ('FRESH TROPICAL FRUIT TEA', 15.99, 'images/Chatea/22.png'),
+                            ('FRESH ORANGE TEA', 15.99, 'images/Chatea/23.png'),
+                            ('FRESH KIWI TEA', 15.99, 'images/Chatea/24.png');";
 
 if ($conn->query($sql_insert)) {
     echo "Tea data added into database<br>";
--- order.php
+++ order.php
@@ -9,14 +9,98 @@
     <link href="https://fonts.googleapis.com/css2?family=Exo:wght@400;600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
+
+<style>
+    body{
+        color: white;
+    }
+
+    img{
+        width: 10%;
+    }
+</style>
 <body>
     <?php
-    $image = $_POST["item_image"];
+    
     $tea_id = $_POST["item_id"];
     $quantity = $_POST["quantity"];
     $ice_level = $_POST["ice_level"];
     $sugar_level = $_POST["sugar_level"];
+    $total = 0.0;
     
+    require_once("config.php");
+
+    if(!$conn){
+        die("Connection Fail");
+     }
+
+    $sql_insert_order = "insert into `order`(tea_id, quantity, ice_level, sugar_level) 
+                         values ($tea_id, $quantity, $ice_level, $sugar_level);";
+
+    if(!$conn->query($sql_insert_order)){
+        die("Insert Fail");
+    }
+
+    $sql_get_all_orders = "SELECT 
+                             order_id,
+                             tea.tea_id,
+                             tea.tea_name, 
+                             `order`.quantity, 
+                             `order`.ice_level, 
+                             `order`.sugar_level, 
+                             tea.tea_price,
+                             tea.tea_image
+                          FROM `order`
+                          JOIN tea ON tea.tea_id = `order`.tea_id;
+                         ";
+
+    $orders = $conn->query($sql_get_all_orders);
+    if(!$orders){
+        die("Query Fail");
+    }
+
+    if ($orders->num_rows > 0) {
+        echo "<table border = '1'>";
+        echo "<colgroup>
+                <col style='width: 60px;'>
+                <col style='width: 150px;'>
+                <col style='width: 100px;'>
+                <col style='width: 100px;'>
+                <col style='width: 80px;'>
+                <col style='width: 100px;'>
+             </colgroup>";
+        echo "<tr>";
+        echo "<th>Image</th>";
+        echo "<th>Tea Name</th>";
+        echo "<th>Ice Level</th>";
+        echo "<th>Sugar Level</th>";
+        echo "<th>Quantity</th>";
+        echo "<th>Price</th>";
+        echo "</tr>";
+
+        while($row = $orders->fetch_assoc()){
+            
+            echo "<tr>"; 
+            echo "<td><img src = '{$row['tea_image']}' style = 'width : 50px' alt = '{$row['tea_name']}'></td>";
+            echo "<td>{$row['tea_name']}</td>";
+            echo "<td>{$row['ice_level']}</td>";
+            echo "<td>{$row['sugar_level']}</td>";
+            echo "<td>{$row['quantity']}</td>";
+            echo "<td>{$row['tea_price']}</td>";
+            echo "</tr>";
+
+            $total += $row['tea_price'] * $row['quantity'];
+        }
+
+        echo "</table>";
+
+        echo "Total: RM ". number_format( $total,2) ."<br>";
+        
+    }else{
+        echo "No Orders Found<br>";
+    }
+
+    $conn->close();
     ?>
         
     
