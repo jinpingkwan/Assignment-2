f35f728ba63102850f7253a9724012964965c42f
jinpingkwan <147835982+jinpingkwan@users.noreply.github.com>
2025-06-12 17:34:22 +0800

    order receipt

--- order.php
+++ order.php
@@ -26,7 +26,7 @@
     $quantity = $_POST["quantity"];
     $ice_level = $_POST["ice_level"];
     $sugar_level = $_POST["sugar_level"];
-    $total = 0.0;
+    
     
     require_once("config.php");
 
@@ -41,66 +41,12 @@
         die("Insert Fail");
     }
 
-    $sql_get_all_orders = "SELECT 
-                             order_id,
-                             tea.tea_id,
-                             tea.tea_name, 
-                             `order`.quantity, 
-                             `order`.ice_level, 
-                             `order`.sugar_level, 
-                             tea.tea_price,
-                             tea.tea_image
-                          FROM `order`
-                          JOIN tea ON tea.tea_id = `order`.tea_id;
-                         ";
-
-    $orders = $conn->query($sql_get_all_orders);
-    if(!$orders){
-        die("Query Fail");
-    }
-
-    if ($orders->num_rows > 0) {
-        echo "<table border = '1'>";
-        echo "<colgroup>
-                <col style='width: 60px;'>
-                <col style='width: 150px;'>
-                <col style='width: 100px;'>
-                <col style='width: 100px;'>
-                <col style='width: 80px;'>
-                <col style='width: 100px;'>
-             </colgroup>";
-        echo "<tr>";
-        echo "<th>Image</th>";
-        echo "<th>Tea Name</th>";
-        echo "<th>Ice Level</th>";
-        echo "<th>Sugar Level</th>";
-        echo "<th>Quantity</th>";
-        echo "<th>Price</th>";
-        echo "</tr>";
-
-        while($row = $orders->fetch_assoc()){
-            
-            echo "<tr>"; 
-            echo "<td><img src = '{$row['tea_image']}' style = 'width : 50px' alt = '{$row['tea_name']}'></td>";
-            echo "<td>{$row['tea_name']}</td>";
-            echo "<td>{$row['ice_level']}</td>";
-            echo "<td>{$row['sugar_level']}</td>";
-            echo "<td>{$row['quantity']}</td>";
-            echo "<td>{$row['tea_price']}</td>";
-            echo "</tr>";
-
-            $total += $row['tea_price'] * $row['quantity'];
-        }
-
-        echo "</table>";
-
-        echo "Total: RM ". number_format( $total,2) ."<br>";
-        
-    }else{
-        echo "No Orders Found<br>";
-    }
+ 
 
     $conn->close();
+
+    header("Location: receipt.php");
+    exit();
     ?>
         
     
